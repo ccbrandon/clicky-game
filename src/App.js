b29c2cfc0342d6a3038236f12a9b87f9b017f0dd
Chris Brandon <ccbrandon@aol.com>
2019-08-16 11:29:55 -0500

    last push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Image from "./components/Image";
+import "./app.css";
+import friends from "./friends.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+    state = {
+        score: 0,
+        images:friends,
+        images_clicked:[]
+    }
+
+    clickme = (id) => {
+        if (this.state.images_clicked.indexOf(id)===-1) {
+            //image doesn't exist
+            this.setState({
+                score:this.state.score + 1,
+                images_clicked: [...this.state.images_clicked, id],
+                images: this.state.images.sort(() => Math.random() - 0.5)
+            })
+
+        } else {
+            this.setState({
+                score: 0,
+                images_clicked: [],
+                images: this.state.images.sort( () => Math.random() - 0.5)
+            })
+        }
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="score">CLICKY GAME SCORE: {this.state.score}</div>
+                {this.state.images.map( (img) => ( 
+                    <Image id={img.id} alt={img.name} src={img.image} key={img.id} onclick={this.clickme} /> )
+                )}
+            </div>
+        )
+    }
 }
 
 export default App;
